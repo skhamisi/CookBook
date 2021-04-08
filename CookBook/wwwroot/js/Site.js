a5d67125053958b8448a8ea4c4f85d4290227c01
Samer Khamisi <samer.n.khamisi@outlook.com>
2021-04-08 09:13:01 -0400

    Initial commit

---
+++ CookBook/wwwroot/js/Site.js
@@ -0,0 +1,22 @@
+﻿// Write your JavaScript code.
+const api = "4e03be6d29fe48f4bb3f8c037d930808"
+const submitButton = document.querySelector("#submit");
+//var searchBarValue = $('#searchbar').val()
+submitButton.addEventListener('click', function () {
+    console.log('You clicked Me!');
+    callRecipe();
+});
+
+function callRecipe() {
+    let viewNumber = 10;
+    let searchBarValue = document.querySelector('#searchbar').value
+    let url = 'https://api.spoonacular.com/recipes/complexSearch?query=' + searchBarValue + '&number=' + viewNumber + '&apiKey='+ api
+    fetch(url)
+        .then(response => (response.json()))
+        .then(response => (console.log(response)))
+
+        
+    //    .then(data => assignCurrentWeatherData(data))
+    console.log("searchBarValue:", searchBarValue)
+    console.log("API query: ", url)
+}
